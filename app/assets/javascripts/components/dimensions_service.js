a304ee6d10fa94f32ef8a7426f87bebbe841ee69
cjvirtucio87 <cjv287@gmail.com>
2016-10-23 08:45:41 -0700

    Added dimensions service

---
+++ app/assets/javascripts/components/dimensions_service.js
@@ -0,0 +1,32 @@
+app.factory('DimensionsService',
+['ComponentService', '_', function (ComponentService, _) {
+  var srv = {};
+  var _data = {
+    widths: [],
+    heights: []
+  };
+
+  function _cacheWidths () {
+    for (var i = 1; i < 12; i++) {
+      _data.widths.push('col-md' + i);
+    }
+  }
+
+  function _cacheHeights () {
+    for (var i = 1; i < 5; i++) {
+      _data.heights.push(i + 'em');
+    }
+  }
+
+  srv.getWidths = function () {
+    if (_.isEmpty(_data.widths)) _cacheWidths();
+    return _data.widths;
+  };
+
+  srv.getHeights = function () {
+    if (_.isEmpty(_data.heights)) _cacheHeights();
+    return _data.heights;
+  };
+
+  return srv;
+}]);
