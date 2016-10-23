8d224de61262f79de9e0dcf588bd5a5b28d62623
cjvirtucio87 <cjv287@gmail.com>
2016-10-22 17:01:50 -0700

    Added migrations for project title and description

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161022201700) do
+ActiveRecord::Schema.define(version: 20161023000128) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,8 +27,10 @@ ActiveRecord::Schema.define(version: 20161022201700) do
 
   create_table "projects", force: :cascade do |t|
     t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.string   "title"
+    t.text     "description"
     t.index ["user_id"], name: "index_projects_on_user_id", using: :btree
   end
 
