0e66eb88aed2a75d3a05e50427b463345bc8fa16
jsteenb2 <jonathansteenbergen@gmail.com>
2016-10-26 08:45:40 -0700

    added base files to header

--- app/models/file_builder.rb
+++ app/models/file_builder.rb
@@ -1,9 +1,42 @@
-require 'open-uri'
+require 'Nokogiri'
 
 class FileBuilder
+  CDN_HASH = {
+    css: {
+      names: ['//fonts.googleapis.com/css?family=Roboto:300,400,500,700"',
+        '//fonts.googleapis.com/icon?family=Material+Icons',
+        'bootstrap.min.css',
+        "bootstrap-material-design.min.css",
+        "ripples.min.css"],
+      rel: "stylsheet"
+    },
+    js: {
+      names: ["jquery-1.12.4.min.js",
+              "bootstrap.min.js",
+              "material.min.js",
+              "ripples.min.js"]
+    }
+  }
 
   def initialize(project_id)
     @project = Project.find_by_id(project_id)
   end
 
+    @builder = Nokogiri::HTML::Builder.new(:encoding => 'UTF-8') do |doc|
+
+      doc.html{
+        doc.head{
+          CDN_HASH[:css][:names].each do |link|
+            doc.link["href"] = "css/#{link}"
+          end
+          CDN_HASH[:js][:names].each do |link|
+            doc.script["src"] = "js/#{link}"
+          end
+        }
+        doc.body {
+          doc.div["class"] = "container"
+        }
+      }
+
+    end
 end
