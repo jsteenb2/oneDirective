11c8f8dac6f932c2de796e35fa4d4d35f73c13f7
cjvirtucio87 <cjv287@gmail.com>
2016-10-27 11:28:29 -0700

    Fixed missing placeholder upon creation

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -17,7 +17,7 @@ class ProjectsController < ApplicationController
     @project = current_user.projects.build(project_params)
     if @project.save
       respond_to do |format|
-        format.json { render json: @project, status: 200 }
+        format.json { render json: @project.to_json(methods: [:photo_url]), status: 200 }
       end
     end
   end
