8f5b661309934e99a0eb8556c121f73d0f4178c6
cjvirtucio87 <cjv287@gmail.com>
2016-10-27 10:24:43 -0700

    added save behavior

--- app/assets/javascripts/nav/nav_controller.js
+++ app/assets/javascripts/nav/nav_controller.js
@@ -3,14 +3,31 @@ app.controller('navbarCtrl', ["$scope", "$rootScope", 'currUser', '$stateParams'
   $scope.currentUser = currUser;
 
   $scope.saveProject = function(ev){
+    $scope.saving = true;
     ev.preventDefault();
     return ProjectService.saveProjectEdits($stateParams.id)
       .then(function(response){
-        console.log(response);
+        _toggleSave();
+        _success();
         return response;
       })
       .catch(function(reason){
         console.log(reason);
       });
   };
+
+  // saving UX
+  $scope.saving = false;
+
+  function _toggleSave () {
+    $scope.saving = !$scope.saving;
+  }
+
+  function _success () {
+    angular.element('span.save-success')
+      .fadeIn(1000)
+      .delay(3000)
+      .fadeOut(1000)
+      .removeClass('save-success');
+  }
 }]);
