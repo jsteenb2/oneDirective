36d2b4c36e855dcd6707e0cadcf1db107f963afe
jsteenb2 <jonathansteenbergen@gmail.com>
2016-10-26 21:26:35 -0700

    working project file builder

--- app/models/file_builder.rb
+++ app/models/file_builder.rb
@@ -7,8 +7,7 @@ class FileBuilder
         '//fonts.googleapis.com/icon?family=Material+Icons',
         'bootstrap.min.css',
         "bootstrap-material-design.min.css",
-        "ripples.min.css"],
-      rel: "stylsheet"
+        "ripples.min.css"]
     },
     js: {
       names: ["jquery-1.12.4.min.js",
@@ -20,35 +19,119 @@ class FileBuilder
 
   def initialize(project_id)
     @project = Project.includes(rows: :components).find_by_id(project_id)
+    @rows = @project.rows
+    @builder = Nokogiri::HTML::Builder.new {
+      html('language' => 'en'){
+        @fs_parent = parent;
+      }
+
+      def fs_parent
+        return @fs_parent;
+      end
+    }
     build_html
     build_html_file
   end
 
   def build_html
-    build_shell
-    puts @builder.to_html
+    add_head(@builder.fs_parent)
+    add_body(@builder.fs_parent)
   end
 
-  def build_shell
-    @builder = Nokogiri::HTML::Builder.new(:encoding => 'UTF-8') do |doc|
-      doc.html{}
-    end
+  def builder
+    return @builder
   end
 
-  def add_head
-    head = Nokogiri::HTML::Builder.new do |doc|
-      doc.head{
-        CDN_HASH[:css][:names].each do |link|
-          doc.link["href"] = "css/#{link}"
-        end
-        CDN_HASH[:js][:names].each do |link|
-          doc.script["src"] = "js/#{link}"
-        end
+  def add_body(fs_parent)
+    fbuilder = Nokogiri::HTML::Builder.new {
+      body {
       }
-      @builder.add_child(head)
+    }
+
+    @rows.each do |row|
+      add_row_with_components(row, fbuilder.doc.root)
+    end
+
+    fs_parent.add_child(fbuilder.doc.root)
+  end
+
+  def add_head(fs_parent)
+    fbuilder = Nokogiri::HTML::Builder.new {
+      head {
+      }
+    }
+    print "pre: #{fbuilder.doc}\n"
+
+    CDN_HASH[:css][:names].each do |link|
+      add_link_tags(link, fbuilder.doc.root)
+    end
+
+    CDN_HASH[:js][:names].each do |link|
+      add_script_tags(link, fbuilder.doc.root)
+    end
+
+    print "after head: #{fbuilder.doc}\n"
+
+    fs_parent.add_child(fbuilder.doc.root)
+    print "post: #{fbuilder.doc}\n"
+  end
+
+  def add_link_tags(link, head_parent)
+    fbuilder = Nokogiri::HTML::Builder.new {
+      link("href" => "css/#{link}", "ref" => "stylesheet"){
+      }
+    }
+
+    head_parent.add_child(fbuilder.doc.root)
+  end
+
+  def add_script_tags(link, head_parent)
+    fbuilder = Nokogiri::HTML::Builder.new {
+      script("src" => "js/#{link}"){
+      }
+    }
+
+    head_parent.add_child(fbuilder.doc.root)
+  end
+
+  def add_row_with_components(row, body_parent)
+    if row.components.first[:component_type] == "nav"
+      build_nav_row(row, body_parent)
+    else
+      fbuilder = Nokogiri::HTML::Builder.new {
+        div("class" => "container"){
+        }
+      }
+      add_row(row.components, fbuilder.doc.root)
+      print "#{fbuilder.doc}\n"
+      body_parent.add_child(fbuilder.doc.root)
     end
   end
 
+  def build_nav_row(row, body_parent)
+    nav_content = row.components.shift
+    body_parent.add_child(nav_content[:content])
+
+    if row.components.length > 0
+      jbuilder = Nokogiri::HTML::Builder.new {
+        div("class" => "container"){
+        }
+      }
+      add_row(row.components, jbuilder.doc.root)
+      body_parent.add_child(jbuilder.doc.root)
+    end
+  end
+
+  def add_row(components, container_parent)
+    fbuilder = Nokogiri::HTML::Builder.new {
+      div("class" => "row"){
+      }
+    }
+    components.each do |component|
+      fbuilder.doc.root.add_child(component[:content])
+    end
+    container_parent.add_child(fbuilder.doc.root)
+  end
 
   def build_html_file
     html = @builder.to_html
